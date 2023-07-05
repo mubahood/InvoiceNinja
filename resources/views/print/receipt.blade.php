aa1134e83f8ae40cf0440ee9c89221d78f0fde38
Muhindo Mubaraka <mubs0x@gmail.com>
2023-07-06 02:24:09 +0300

    receipt

--- resources/views/print/receipt.blade.php
+++ resources/views/print/receipt.blade.php
@@ -20,6 +20,7 @@ $sign = public_path('/sign.jpg');
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ public_path('css/bootstrap-print.css') }}">
     <title>Payment receipt</title>
+
 </head>
 
 <body>
@@ -33,7 +34,7 @@ $sign = public_path('/sign.jpg');
                     </td>
                     <td class=" text-left">
                         <p class="p-0 m-0" style="font-size: 1.3rem;"><b>NICSIM PROPERTY MANAGERS LIMITED</b></p>
-                        <p class="mt-1">P.O.BOC: <b>27063 - KAMPALA</b>
+                        <p class="mt-1">P.O.BOX: <b>27063 - KAMPALA</b>
                         <p class="mt-1">Tel: <b>+256708180880</b> , <b>+256775280880</b>
                         </p>
                     </td>
@@ -50,7 +51,7 @@ $sign = public_path('/sign.jpg');
         <p class="text-right mb-4"><b>{{ Utils::my_date($receipt->created_at) }}</b></p>
         <p>Received sum of <b>UGX {{ number_format($receipt->amount) }}</b> in words:
             <b>{{ Utils::convert_number_to_words($receipt->amount) }}</b>.
-            <p>{{ $receipt->details }} </p>
+        <p>{{ $receipt->details }} </p>
         </p>
         <p class="mt-3 mb-4">BALANCE: <b>UGX {{ number_format($receipt->balance) }}</b></p>
 
