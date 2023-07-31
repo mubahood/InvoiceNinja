2a5f11c55be6d6e279d01bdf254a7e96d87c381c
SumayahSwaib <sumayahswaibk@gmail.com>
2023-07-31 14:30:17 +0300

    application

---
+++ resources/views/print/application.blade.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-print.css">
+    <title>Application Form </title>
+</head>
+<body>
+  <div class="container text-center">
+    <div class="row align-items-center">
+     
+    <div class="col-5"><h4><strong>To be filled in Triplicates</strong></h4></div>
+    <div class="col-2"> <img src="/logo.jpg" alt="logo"></div>
+    <div class="col-5"> <h5><strong>FORM TAT 1</strong></h5></div><br>
+  
+    
+   
+    </div>
+ 
+    <div class="text-center"> <p>THE REPUBLIC OF UGANDA</p></div><br>
+    <hr style="background-color: rgb(26, 9, 94); height: 3px;" class="p-0 m-0 mt-0 mb-4">
+
+
+    <form action="" method="get">
+      <div class="form-group">
+      <label for="registry">IN THE TAX APPEALS TRIBUNAL AT (REGISTRY):</label> <input type="text" class="form-control " id="registry" name="registry"><br>
+    </div>
+    <div class="row align-items-center">
+    <div class="col-6">
+      <div class="form-group">
+        <label for="APPLICATION NO">APPLICATION NO:</label>
+        <input type="text" class="form-control" id="applicationNo" name="applicationNo"><br>
+      </div>
+    </div>
+    <div class="col-6">
+      <label for="year">YEAR:</label>
+      <input type="year" class="form-control" id="year" name="year"><br>
+      
+    </div>
+
+    </div>
+    
+      
+      
+    <div class="form-group">
+      <label for="applicant">IN THE  MATTER OF(APPLICANT):</label>
+      <input type="text" class="form-control" id="applicant" name="applicant"><br>
+    <h5><strong>AND</strong></h5><br>
+    <div class="form-group">
+      <label for="respondent">IN THE  MATTER OF(RESPONDENT):</label>
+      <input type="text"class="form-control" id="respondent" name="respondent"><br>
+    </div>
+    <hr style="background-color: rgb(26, 9, 94); height: 3px;" class="p-0 m-0 mt-0 mb-4">
+
+     
+
+      
+
+      <h4 class="text-ceneter">APPLICATION</h4>
+      <p>Under Section 17 of the Act and rule 10</p><br>
+<div class="row align-center">
+<div class="col-4">
+  <label for="name">(a) Name:</label> <input type="text" class="form-control" id="name" name="name"><br>
+</div>
+<div class="col-4">
+  <label for="nature">(b) Nature of Business:</label> <input type="text" class="form-control" id="nature" name="nature"><br>
+</div>
+<div class="col-4">
+  <label for="PostalAddress">(c) PostalAddress:</label> <input type="text" class="form-control" id="PostalAddress" name="PostalAddress"><br>
+</div>
+</div>
+     
+    
+     <p>(d) Physical Address</p><br>
+     <div class="row">
+     <div class="col-4">
+      <label for="plot"> Plot No:</label> <input type="text" class="form-control" id="plot" name="plot"><br>
+     </div>
+    <div class="col-4">
+      <label for="plot"> Plot No:</label> <input type="text" class="form-control" id="plot" name="plot"><br>
+    </div>
+    <div class="col-4">
+      <label for="street"> Street:</label> <input type="text" class="form-control"  id="street" name="street"><br>
+    </div>
+     </div>
+     
+     <div class="row">
+    <div class="col-6">
+      <label for="village"> Village</label> <input type="text"class="form-control" id="village" name="village"><br>
+   </div>
+    <div class="col-6">
+      <label for="town"> Trading Center/Town/City:</label> <input type="text" class="form-control"id="town" name="town"><br>
+   </div>
+     </div>
+     <div class="row">
+      <div class="col-4">
+        <label for="telephone">(e) Telephone:</label> <input type="text" class="form-control" id="telephone" name="telephone"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="fax"> Fax No:</label> <input type="text" class="form-control" id="fax" name="fax"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="fax"> Email:</label> <input type="email" class="form-control" id="email" name="email"><br>
+
+      </div>
+
+     </div>
+
+     <div class="row">
+      <div class="col-4">
+        <label for="tin"> (f)TIN:</label> <input type="text" class="form-control" id="tin" name="tin"><br>
+
+      </div>
+      
+      <div class="col-4">
+        <label for="IncomeTaxFile"> (g)Income Tax File No:</label> <input type="text"class="form-control" id="IncomeTaxFile" name="IncomeTaxFile"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="VatNumber">(h) Vat Number (if registered):</label> <input type="text" class="form-control" id="VatNumber" name="VatNumber"><br><br>
+
+      </div>
+
+     </div>
+     <hr style="background-color: rgb(26, 9, 94); height: 3px;" class="p-0 m-0 mt-0 mb-4">
+
+     
+
+
+
+     <h4 class="text-ceneter">2 PARTICULARS OF THE TAX DISPUTES</h4><br>
+     <label for="office"> (a)Office where the tax decision is made:</label> <input type="text" class="form-control"  id="office" name="office"><br>
+
+     <p>(b)Type of tax specify by a tick in the box below as appropriate</p><br>
+     <div class="row">
+      <div class="col-4">
+        <label for="IncomeTax"> INCOME TAX:</label> <input type="checkbox" id="IncomeTax" name="IncomeTax"><br>
+        <label for="ExciseDuty"> EXCISE DUTY:</label> <input type="checkbox" id="ExciseDuty" name="ExciseDuty"><br>
+        <label for="ImportCommision"> IMPORT COMMISION:</label> <input type="checkbox" id="ImportCommision" name="ImportCommision"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="others"> OTHERS:</label> <input type="checkbox" id="others" name="others"><br>
+        <label for="ImportDuty"> IMPORT DUTY:</label> <input type="checkbox" id="ImportDuty" name="ImportDuty"><br>
+
+        
+      </div>
+      <div class="col-4">
+        <label for="WithholdingTax"> WITHHOLDING TAX:</label> <input type="checkbox" id="WithholdingTax" name="WithholdingTax"><br>
+        <label for="VAT"> VAT:</label> <input type="checkbox" id="VAT" name="VAT"><br>
+     </div>
+
+     </div>
+     <div class="row">
+      <div class="col 4">
+        <label for="AssessmentNo"> (c)Assessment No:</label> <input type="text" class="form-control" id="AssessmentNo" name="AssessmentNo"><br>
+
+      </div>
+      <div class="col 4">
+        <label for="CustomsBill">(d) Customs Bill of Entry</label> <input type="text" class="form-control" id="CustomsBill" name="CustomsBill"><br>
+
+      </div>
+
+     </div>
+     <div class="row">
+      <div class="col-4">
+        <label for="BankPaymentAdviceFormNo">(e) Bank Payment Advice Form No</label> <input type="text"class="form-control" id="BankPaymentAdviceFormNo" name="BankPaymentAdviceFormNo"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="AmountOfTax">(f) Amount of tax in dispute or objected to:</label> <input type="text" class="form-control" id="AmountOfTax" name="AmountOfTax"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="DateOfService"> (g)Date of services of taxation decision :</label> <input type="date" class="form-control"id="DateOfService" name="DateOfService"><br>
+
+      </div>
+
+     </div>
+
+     <div class="row">
+      
+
+     </div>
+     
+     
+     <hr style="background-color: rgb(26, 9, 94); height: 3px;" class="p-0 m-0 mt-0 mb-4">
+
+     <h4 class="text-center">3 STATEMENT OF FACT AND REASONS IN SUPPORT OF THE APPLICATION</h4>
+     <P>if space provided is not adqate, attach as many additional pages as needed for the statement </P><br>
+     <input type="textarea" id="StatementOfFacts" class="form-control" name="StatementOfFacts" rows="19" cols="50"><br>
+
+     <h5 class="text-center">4 ISSUES ON WHICH A DECISION/ARE SOUGHT</h5><br>
+     <input type="textarea"class="form-control" id="Issues" name="Issues"><br>
+
+     <h5 class="text-center">5 LIST OF BOOKS, DOCUMENTS FOR THINGS TO BE PRODUCES BEFORE THE TRIBUNAL IF ANY</h5>
+     <P>Give brief description of each </P><br>
+     <input type="textarea"class="form-control" id="books" name="books"><br>
+
+     <h5 class="text-center">6 NAMES OF WITNESSES IF ANY AND THEIR ADDRESS</h5><br>
+
+     <input type="textarea"class="form-control" id="books" name="books" rows="4" cols="50"><br>
+     <div class="row">
+      <div class="col-4">
+        <label for="Date"> Dated this:</label> <input type="text" class="form-control" id="date" name="date"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="month"> Month</label> <input type="text" class="form-control" id="Month" name="Month"><br>
+
+      </div>
+      <div class="col-4">
+        <label for="Year"> Year:</label> <input type="text" class="form-control" id="Year" name="Year"><br>
+
+      </div>
+
+     </div>
+     <p>.................................................... </p>
+     <p>Signature of Application/ Advocate for the </p>
+    <p> Application/ Aagent of applicant </p><br><br>
+
+     <h4>(FOR OFFICIAL USE)</h4><br>
+     <p><strong>7.By Registrar / officer - in- charge</strong></p><br>
+
+
+    <p>Date of filling of application:.................................. </p> <br>
+    <p>Signature:..................................................... </p> <br>
+
+    <p>offical Stamp of Registrar/ officer un charge</p><br>
+
+    <p><strong>8. By Commissioner General</strong></p>
+    <p>Service of copy of application on Commisiion General</p><br>
+
+    <p>Date :..................................................... </p> <br>
+    <p>Sign:..................................................... </p> <br>
+
+
+      <input type="submit" value="Submit" class="btn btn-sucess">
+
+     </form>
+  </div>
+ 
+
+</body>
+</html>
