11a8c96754e3d9e8f50aa3762a734b35a8699787
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-03 08:59:28 +0300

    Application

--- app/Admin/Controllers/ApplicationController.php
+++ app/Admin/Controllers/ApplicationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Admin\Controllers;
 
-use App\models\Application;
+use App\Models\Application;
 use Encore\Admin\Controllers\AdminController;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
