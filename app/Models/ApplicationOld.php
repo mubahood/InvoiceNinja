542b0b8c16952ee1e0895665dc18652784343bf3
Muhindo Mubaraka <mubs0x@gmail.com>
2023-08-03 08:58:12 +0300

    new php artisan make:model Application

--- app/Models/ApplicationOld.php
+++ app/Models/ApplicationOld.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Application extends Model
+class ApplicationOld extends Model
 {
     use HasFactory;
+
+    public function attarchments()
+    {
+        return $this->hasMany(Attarchment::class);
+    }
 }
